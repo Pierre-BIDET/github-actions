1b4c03fd99792586f2aa70f224f594da822221ee
Pierre Bidet <pierre.bidet@jobteaser.com>
2023-05-29 23:26:51 +0200

    test file modifs for Flake8 compliance v2

--- file_test.py
+++ file_test.py
@@ -1,5 +1,3 @@
-import pytest
-
 def test_calc_addition():
     # Fonction test du résultat de 2+4
     output = 2+4
@@ -17,5 +15,5 @@ def test_calc_multiply():
 
 def test_coucou():
     # Fonction test si la résultat renvoie 'hello'
-    output='hello'
+    output = 'hello'
     assert output == 'hello'
